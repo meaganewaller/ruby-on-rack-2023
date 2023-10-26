60e100c40640b79569664de17c09ee3fb9515048
Meagan Waller <meagan.waller@testdouble.com>
2023-10-25 21:04:05 -0400

    ✨ Feat (routing): Implement Basic Routing Functionality
    In this commit, we introduce basic routing functionality to our microframework in the "step-2-basic-routing" branch.
    The core changes are as follows:
    
    - Added routing logic to direct requests to different endpoints:
      - Requests to "/hello" are handled by the hello_response method.
      - Requests to "/about" are handled by the about_response method.
      - Requests to unknown routes return a "Not Found" response.
    - Each route handler method constructs an appropriate HTTP response.

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 class MyApp
-  def call(_env)
+  def call(env)
+    request_path = env['PATH_INFO']
+
+    if request_path == '/hello'
+      return hello_response
+    elsif request_path == '/about'
+      return about_response
+    else
+      return not_found_response
+    end
+  end
+
+  def hello_response
     [200, { 'Content-Type' => 'text/html' }, ['Hello, San Diego!']]
+  end
+
+  def about_response
+    [200, { 'Content-Type' => 'text/html' }, ['About Page']]
+  end
+
+  def not_found_response
+    [404, { 'Content-Type' => 'text/html' }, ['Not Found']]
   end
 end
