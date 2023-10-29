883bae15490c74f31433cc4d0e62df85e92bf1bd
Meagan Waller <meagan.waller@testdouble.com>
2023-10-29 18:39:53 -0400

    ✨ Feat(app.rb): Add create and view post functionality
    - :rocket: Introduces new routes and functionality for creating and viewing posts.
    - :white_check_mark: Adds unit tests for the `create_post` and `view_posts` routes.
    - :wrench: Defines the `format_posts` method for post formatting in the `MyApp` class.

--- app.rb
+++ app.rb
@@ -26,12 +26,47 @@ class MyApp
   def call(env)
     request_path = env['PATH_INFO']
 
-    if request_path == '/hello'
+    case request_path
+    when '/hello'
       hello_response
-    elsif request_path == '/about'
+    when '/about'
       about_response
+    when '/create_post'
+      if env['REQUEST_METHOD'] == 'POST'
+        create_post(env)
+      else
+        [405, { 'Content-Type' => 'text/html' }, ['Method Not Allowed']]
+      end
+    when '/posts'
+      if env['REQUEST_METHOD'] == 'GET'
+        view_posts
+      else
+        [405, { 'Content-Type' => 'text/html' }, ['Method Not Allowed']]
+      end
     else
       not_found_response
+    end
+  end
+
+  private
+
+  def create_post(env)
+    request = Rack::Request.new(env)
+    title = request.params['title']
+    content = request.params['content']
+
+    insert_post(title, content)
+
+    [201, { 'Content-Type' => 'text/html' }, ['Post created']]
+  end
+
+  def view_posts
+    posts = retrieve_posts
+
+    if posts.empty?
+      [200, { 'Content-Type' => 'text/html' }, ['No posts available']]
+    else
+      [200, { 'Content-Type' => 'texthtml' }, [format_posts(posts)]]
     end
   end
 
@@ -45,5 +80,13 @@ class MyApp
 
   def not_found_response
     [404, { 'Content-Type' => 'text/html' }, ['Not Found']]
+  end
+
+
+  def format_posts(posts)
+    formatted_posts = posts.map do |post|
+      "<h2>#{post[1]}</h2><p>#{post[2]}</p>"
+    end
+    formatted_posts.join("\n")
   end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,66 +1,124 @@
-require "rack/test"
-require_relative "../app"
+require 'rack/test'
+require_relative '../app'
 
 describe MyApp do
   include Rack::Test::Methods
 
-  let(:app) { MyApp.new(SQLite3::Database.new(":memory:")) }
+  let(:app) { MyApp.new(SQLite3::Database.new(':memory:')) }
 
-  describe "#initialize" do
-    it "initializes a database connection" do
+  describe '#initialize' do
+    it 'initializes a database connection' do
       expect(app.instance_variable_get(:@db)).to be_a(SQLite3::Database)
     end
   end
 
-  describe "#create_posts_table" do
-    it "creates the posts table" do
+  describe '#create_posts_table' do
+    it 'creates the posts table' do
       expect { app.create_posts_table }.not_to raise_error
     end
   end
 
-  describe "#insert_post" do
-    it "inserts a post" do
-      app.create_posts_table
+  describe '#insert_post' do
+    before { app.create_posts_table }
+
+    it 'does not raise an error' do
+      expect { app.retrieve_posts }.not_to raise_error
+    end
+
+    it 'inserts a post' do
       app.instance_variable_get(:@db).transaction do
-        expect { app.insert_post("Hello, San Diego!", "This is a simple Rack application.") }.not_to raise_error
+        expect { app.insert_post('Hello, San Diego!', 'This is a simple Rack application.') }.not_to raise_error
       end
     end
   end
 
-  describe "#retrieve_posts" do
+  describe '#retrieve_posts' do
     before do
       app.create_posts_table
-      app.insert_post("Hello, San Diego!", "This is a simple Rack application")
-      app.insert_post("Another Post!", "This is another post")
+      app.insert_post('Hello, San Diego!', 'This is a simple Rack application')
+      app.insert_post('Another Post!', 'This is another post')
     end
 
-    it "does not raise an error" do
+    it 'does not raise an error' do
       expect { app.retrieve_posts }.not_to raise_error
     end
 
-    it "returns all posts" do
-      expect(app.retrieve_posts).to eq([
-        [1, "Hello, San Diego!", "This is a simple Rack application"],
-        [2, "Another Post!", "This is another post"]
-      ])
+    it 'returns all posts' do
+      expect(app.retrieve_posts).to eq(
+        [
+          [1, 'Hello, San Diego!', 'This is a simple Rack application'],
+          [2, 'Another Post!', 'This is another post']
+        ]
+      )
+    end
+  end
+
+  describe '#create_post' do
+    it 'inserts a post' do
+      app.create_posts_table
+
+      post_data = {
+        'title' => 'New Post Title',
+        'content' => 'This is the content of a new post.'
+      }
+
+      post '/create_post', post_data
+
+      expect(last_response).to be_created
+      expect(last_response.body).to include('Post created')
+    end
+
+    it 'returns Method Not Allowed for non-POST requests' do
+      get '/create_post'
+      expect(last_response).to be_method_not_allowed
+      expect(last_response.body).to include('Method Not Allowed')
+    end
+  end
+
+  describe '#view_posts' do
+    it 'displays available posts' do
+      app.create_posts_table
+      app.insert_post('Test Post 1', 'This is the first test post.')
+      app.insert_post('Test Post 2', 'This is the second test post.')
+
+      get '/posts'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('Test Post 1')
+      expect(last_response.body).to include('Test Post 2')
+    end
+
+    it 'displays a message for no posts available' do
+      app.create_posts_table
+
+      get '/posts'
+
+      expect(last_response).to be_ok
+      expect(last_response.body).to include('No posts available')
+    end
+
+    it 'returns Method Not Allowed for non-GET requests' do
+      post '/posts'
+      expect(last_response).to be_method_not_allowed
+      expect(last_response.body).to include('Method Not Allowed')
     end
   end
 
   it 'responds with "Hello, San Diego!" for /hello' do
-    get "/hello"
+    get '/hello'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq("Hello, San Diego!")
+    expect(last_response.body).to eq('Hello, San Diego!')
   end
 
   it 'responds with "This is a simple Rack application." for /about' do
-    get "/about"
+    get '/about'
     expect(last_response).to be_ok
-    expect(last_response.body).to eq("This is a simple Rack application.")
+    expect(last_response.body).to eq('This is a simple Rack application.')
   end
 
   it 'responds with "Not Found" for an unknown path' do
-    get "/unknown"
+    get '/unknown'
     expect(last_response).to be_not_found
-    expect(last_response.body).to eq("Not Found")
+    expect(last_response.body).to eq('Not Found')
   end
 end
